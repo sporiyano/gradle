ec1a77f36cec2f4068eefcd43304655efd9a657a
Benjamin Muskalla <bmuskalla@gradle.com>
2020-08-14 14:35:06 +0200

    Move installation suppliers to jvm-services

--- subprojects/platform-jvm/platform-jvm.gradle.kts
+++ subprojects/platform-jvm/platform-jvm.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation(project(":diagnostics"))
     implementation(project(":normalization-java"))
     implementation(project(":resources"))
+    implementation(project(":jvm-services"))
 
     implementation(libs.groovy)
     implementation(libs.guava)
